c57a20e1fc34b1c6794ba6964c7c734fc39c5139
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2022-10-24 09:06:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+################ Define Scope 1 Emissions (Indirect) ################
+
+## Stationary Combustion
+### What type of Stationary Combustion?
+stationary = input("Type: ")
+#### What metric?
+metric = st.selectbox('How would you like to be contacted?', ('Hour','Day','Week','Month',
+                                                              'Year'))
+##### What is the time period
+# Add new
+
+## Stationary Combustion
+### Type
+#### Metric
+##### Time period
+# Add new
+
+## Stationary Combustion
+### Type
+#### Metric
+##### Time period
+# Add new
+
+## Stationary Combustion
+### Type
+#### Metric
+##### Time period
+# Add new
+
+################ Define Scope 2 Emissions (Direct) ################
+
+
+
+
+################ Define Scope 3 Emissions (Indirect) ################
