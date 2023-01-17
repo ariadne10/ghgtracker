0c9a888ef92077bcca2481ce60816217e053f323
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2023-01-17 13:34:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,32 +5,22 @@ import numpy as np
 
 # Create a function that returns the options for the second dropdown based on the selected value of the first dropdown
 @st.cache
-def get_options_2(value):
-    if value == 'VBC Osage':
-        return ['FY20', 'FY21', 'FY22', 'FY23', 'FY24', 'FY25', 'FY26']
-    elif value == 'VBC Libertyville':
-        return ['FY20', 'FY21', 'FY22', 'FY23', 'FY24', 'FY25', 'FY26']
-    else:
-        return []
+def get_options_2():
+    return ['FY20', 'FY21', 'FY22', 'FY23', 'FY24', 'FY25', 'FY26']
 
 # Create a function that returns the options for the third dropdown based on the selected value of the second dropdown
 @st.cache
-def get_options_3(value):
-    if value == 'Option 2.1':
-        return ['Option 3.1', 'Option 3.2']
-    elif value == 'Option 2.2':
-        return ['Option 3.3', 'Option 3.4']
-    else:
-        return []
+def get_options_3():
+    return ['Scope 1', 'Scope 2', 'Scope 3']
 
 # Create the first dropdown
-options_1 = ['Option 1', 'Option 2']
+options_1 = ['VBC Osage', 'VBC Libertyville', 'Pace International', 'Mycorrhizal Applications', 'MGK', 'Valent USA']
 value_1 = st.selectbox('Select an option', options_1)
 
 # Create the second dropdown
-options_2 = get_options_2(value_1)
+options_2 = get_options_2()
 value_2 = st.selectbox('Select an option', options_2)
 
 # Create the third dropdown
-options_3 = get_options_3(value_2)
+options_3 = get_options_3()
 value_3 = st.selectbox('Select an option', options_3)
